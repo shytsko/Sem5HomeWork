ec0ad5e5d8fa0852efd744228da9281c88a5f0c1
Vladimir Shytsko <shytsko.v@gmail.com>
2022-04-26 21:00:26 +0300

    Add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+Console.Write("Введите размер массива: ");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+int[] array = GenerateArray(sizeArray, 100, 999);
+PrintArray(array, '[', ']', ", ", true);
+int countEvenNumbers = CountEvenNumbers(array);
+Console.WriteLine($"Количество чётных чисел: {countEvenNumbers}");
+
+
+int[] GenerateArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return array;
+}
+
+int CountEvenNumbers(int[] array)
+{
+    int countEven = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            countEven++;
+    }
+
+    return countEven;
+}
+
+void PrintArray(int[] array, char startSymbol, char endSymbol, string separator, bool endLine)
+{
+    Console.Write(startSymbol);
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i != array.Length - 1)
+            Console.Write(separator);
+    }
+    Console.Write(endSymbol);
+    if (endLine)
+        Console.WriteLine();
+}
+
