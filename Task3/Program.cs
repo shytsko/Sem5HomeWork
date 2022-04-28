36aba59727e99fc31b6a10762d90a35c0fc909c3
Vladimir Shytsko <shytsko.v@gmail.com>
2022-04-28 22:35:18 +0300

    Update Task2

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -5,25 +5,46 @@
 
 Console.Write("Введите размер массива: ");
 int sizeArray = Convert.ToInt32(Console.ReadLine());
-double[] array = new double[sizeArray];
-
-for (int i = 0; i < array.Length; i++)
-{
-    Console.Write($"array[{i}] = ");
-    array[i] = Convert.ToDouble(Console.ReadLine());
-}
+double[] array = GenerateDoubleArray(sizeArray, 0, 100);
 
 double min = array[0];
 double max = array[0];
 for (int i = 1; i < array.Length; i++)
 {
-    if (array[i]>max)
+    if (array[i] > max)
         max = array[i];
 
-    if (array[i]<min)
+    if (array[i] < min)
         min = array[i];
 }
 
 double diff = max - min;
-
+PrintArray(array, '[', ']', ", ", true);
 Console.WriteLine($"Разность между максимальным и минимальным элементами массива: {diff}");
+
+
+double[] GenerateDoubleArray(int size, double minValue, double maxValue)
+{
+    double[] array = new double[size];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().NextDouble() * (maxValue - minValue) + minValue;
+    }
+
+    return array;
+}
+
+void PrintArray(double[] array, char startSymbol, char endSymbol, string separator, bool endLine)
+{
+    Console.Write(startSymbol);
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i != array.Length - 1)
+            Console.Write(separator);
+    }
+    Console.Write(endSymbol);
+    if (endLine)
+        Console.WriteLine();
+}
